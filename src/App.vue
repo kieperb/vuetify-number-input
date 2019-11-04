af0881f69eae3ce2d7be8aaf8744c45c3209da99
Benjamin Kieper <36973284+kieperb@users.noreply.github.com>
2019-11-05 00:23:50 +0100

    Format
    Formatierung

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,20 @@
   <div id="app">
     <v-form>
       <v-container>
-        <NumberInput solo  dense placeholder="0" hint="if empty value is 0" label="NumberInput" :numberValue="bla" v-on:emitnumber="bla=$event"/>
+        <NumberInput
+          solo
+          dense
+          placeholder="0"
+          hint="if empty value is 0"
+          label="NumberInput"
+          :numberValue="bla"
+          v-on:emitnumber="bla=$event"
+        />
         App: {{bla}} (is a {{typeof bla}})
-       </v-container>
+      </v-container>
     </v-form>
   </div>
- </template>
+</template>
 
 <script>
 import NumberInput from "./components/NumberInput";
