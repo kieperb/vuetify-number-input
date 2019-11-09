4279280e2a1ae40d4079216ef3a22619da20b449
Benjamin Kieper <36973284+kieperb@users.noreply.github.com>
2019-11-09 15:19:02 +0100

    outlined label is crossed
    fighting with vuetify issues

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,32 @@
   <div id="app">
     <v-form>
       <v-container>
-        <v-row>
-          <NumberInput
-            solo
-            dense
-            placeholder="0"
-            hint="if empty value is 0"
-            label="NumberInput"
-            :value="bla"
-            v-on:emitnumber="bla=$event"
-          />
-        </v-row>
-        <v-row>App: {{bla}} (is a {{typeof bla}})</v-row>
-        <v-row>
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="NumberInput"></NumberInput>
-          </v-col>
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput placeholder="Placeholder" label="NumberInput"></NumberInput>
-          </v-col>
+        <NumberInput
+          solo
+          dense
+          placeholder="0"
+          hint="if empty value is 0"
+          label="NumberInput"
+          :value="bla"
+          v-on:emitnumber="bla=$event"
+        ></NumberInput>
 
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="Solo" solo></NumberInput>
-          </v-col>
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="Solo" placeholder="Placeholder" solo></NumberInput>
-          </v-col>
+        App: {{bla}} (is a {{typeof bla}})
+        <NumberInput label="NumberInput"></NumberInput>
 
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="Filled" filled></NumberInput>
-          </v-col>
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="Filled" placeholder="Placeholder" filled></NumberInput>
-          </v-col>
+        <NumberInput placeholder="Placeholder" label="NumberInput"></NumberInput>
 
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="Outlined" outlined></NumberInput>
-          </v-col>
-          <v-col cols="12" sm="6" md="3">
-            <NumberInput label="Outlined" placeholder="Placeholder" outlined></NumberInput>
-          </v-col>
-        </v-row>
+        <NumberInput label="Solo" solo></NumberInput>
+
+        <NumberInput label="Solo" placeholder="Placeholder" solo></NumberInput>
+
+        <NumberInput label="Filled" filled></NumberInput>
+
+        <NumberInput label="Filled" placeholder="Placeholder" filled></NumberInput>
+
+        <NumberInput label="Outlined" outlined></NumberInput>
+
+        <NumberInput style label="Outlined" placeholder="Placeholder" outlined></NumberInput>
       </v-container>
     </v-form>
   </div>
